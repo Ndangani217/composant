caf87fe4f29ec2284dd98bc8446e04b4b0e08156
Heritier Ndangani <heritierndangani217@gmail.com>
2024-08-16 03:56:53 +0200

    modification du fichier css

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Sélectionne tous les éléments avec la classe "accordion"
-document.querySelectorAll(".accordion").forEach(button => {
-    button.addEventListener("click", function() {
-      // Basculer la classe "active" sur le bouton cliqué
-      this.classList.toggle("active");
+  document.querySelectorAll(".accordion").forEach(button => {
+  button.addEventListener("click", function() {
+       // Basculer la classe "active" sur le bouton cliqué
+    this.classList.toggle("active");
   
-      // Basculer l'affichage du panneau associé
-      const panel = this.nextElementSibling;
-      panel.style.display = panel.style.display === "block" ? "none" : "block";
-    });
+       // Basculer l'affichage du panneau associé
+    const panel = this.nextElementSibling;
+    panel.style.display = panel.style.display === "block" ? "none" : "block";
+  
   });
+});
   
   
